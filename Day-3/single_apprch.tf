77cf30b0e97ffd04e7e998bd0a1a9239289ed2f7
Shiva Sai <57152818+Shiva-7@users.noreply.github.com>
2025-01-19 14:57:43 -0500

    Update single_apprch.tf

--- Day-3/single_apprch.tf
+++ Day-3/single_apprch.tf
@@ -1,1 +1,26 @@
 #lets create the same using a single level approach
+
+#PROVIDER_DETAILS
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "netflix" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+  
+}
+
+#INITIALIZING THE VARIABLES
+variable "ami_value" {
+  description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+#OUTPUTS_DISPLAY
+output "public-ip-address" {
+    value = aws_instance.netflix.public_ip 
+}
